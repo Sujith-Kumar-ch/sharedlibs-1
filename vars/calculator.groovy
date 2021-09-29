e9380b9dc867eca4e28003f3ab27ebe525d823c7
Sujith-Kumar-ch <86654454+Sujith-Kumar-ch@users.noreply.github.com>
2021-09-29 11:18:42 +0530

    Create calculator.groovy

---
+++ vars/calculator.groovy
@@ -0,0 +1,15 @@
+def add(x,y){
+  echo "Sum of ${x} and ${y} is ${x+y} "
+}
+
+def add(x,y){
+  echo "MUL of ${x} and ${y} is ${x*y} "
+}
+
+def add(x,y){
+  echo "Sub of ${x} and ${y} is ${x-y} "
+}
+
+def add(x,y){
+  echo "Div of ${x} and ${y} is ${x/y} "
+}
